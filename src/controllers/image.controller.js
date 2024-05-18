2dcd46ae243d4f3ffa000b2805e9c8db0da895c0
HarukaYamamoto0 <harukayamamotodev@gmail.com>
2024-05-18 19:26:30 -0300

    feat: improved cache, but not enough

--- src/controllers/image.controller.js
+++ src/controllers/image.controller.js
@@ -3,52 +3,57 @@ import { prismaClient } from "../database/prismaClient.js";
 import { redisClient } from "../database/redisClient.js";
 import { FileOperations } from "../utils/FileOperations.js";
 import { cannotBeNull } from "../utils/cannotBeNull.js";
-import { readFile } from "node:fs/promises";
 import { fileTypeFromBuffer } from "file-type";
 import locartConfig from "../../locart.config.js";
 
-const cacheTimeForServer = locartConfig.server.image_cache_time;
-const cacheTimeForClient = locartConfig.database.redis.image_cache_time;
+const cacheTimeForServer = locartConfig.server["image_cache_time"];
+const cacheTimeForClient = locartConfig.database.redis["image_cache_time"];
 
 class ImageController {
   async get(req, res) {
     try {
-      // TODO: This code can still be greatly optimized, but for now it works
       const { id } = req.params;
 
       const isNull = cannotBeNull({ id });
 
       if (isNull[0]) {
-        return res.status(400).json({ error: `Parameter ${isNull[1]} cannot be empty` });
+        return res.status(400).json({ error: "You didn't pass the id" });
       }
 
-      let image = await redisClient.getBuffer(`image-buffer-${id}`);
-      let mimeType = null;
+      const cachedImage = await redisClient.getBuffer(`image-buffer-${id}`);
+      let image, mimeType;
 
-      if (!image) {
-        const imageRedis = await prismaClient.image.findFirstOrThrow({
+      if (!cachedImage) {
+        const imageRedis = await prismaClient.image.findUnique({
           where: { id },
         });
 
         if (!imageRedis) return res.status(400).json({ error: `Image id ${id} does not exist` });
 
-        image = await readFile(imageRedis.path);
+        image = await FileOperations.readFile(imageRedis.path);
         mimeType = imageRedis.mimeType;
 
         res.setHeader("Cache-Control", `public, max-age=${cacheTimeForClient}`);
         res.setHeader("Expires", new Date(Date.now() + cacheTimeForClient).toUTCString());
         res.setHeader("Last-Modified", imageRedis.createdAt.toUTCString());
 
+        mimeType = imageRedis.mimeType;
         await redisClient.setBuffer(`image-buffer-${id}`, image, "EX", cacheTimeForServer);
       }
 
-      const fileType = mimeType != null ? mimeType : await fileTypeFromBuffer(image);
+      if (!mimeType) {
+        const fileType = await fileTypeFromBuffer(image);
+        mimeType = fileType.mime;
+      }
 
-      res.setHeader("Content-Type", fileType.mime);
+      res.setHeader("Content-Type", mimeType);
       res.send(image);
     } catch (error) {
-      console.error("[ImageController-Get] - Error when trying to get the image: " + error.stack);
-      res.status(500).json({ error: `Error when trying to get the image:  ` + error });
+      console.error(`[ImageController-Get] - Error when trying to get the image: ${error.stack}`);
+      res.status(500).json({
+        error:
+          "Error when trying to get the image, try later, but if the error persists between our support",
+      });
     }
   }
 
@@ -66,9 +71,9 @@ class ImageController {
 
       res.send(image);
     } catch (error) {
-      console.error(`[ImageController] - Error when trying to create the image: ` + error);
-      res.status(500).json({ error: `Error when trying to create the image: ` + error });
-      FileOperations.rm(path);
+      console.error(`[ImageController-Create] - Error when trying to create the image: ` + error);
+      res.status(500).json({ error: "Error when trying to create the image" });
+      await FileOperations.rmFile(path);
     }
   }
 
@@ -90,7 +95,7 @@ class ImageController {
         },
       });
 
-      await FileOperations.mv(image.path, path);
+      await FileOperations.moveFile(image.path, path);
 
       const newImage = await prismaClient.image.update({
         where: {
@@ -104,7 +109,7 @@ class ImageController {
       res.send(newImage);
     } catch (error) {
       console.error(`[ImageController] - Error when trying to update the image: ` + error);
-      res.status(500).json({ error: `Error when trying to update the image: ` + error });
+      res.status(500).json({ error: "Error when trying to update the image" });
     }
   }
 
@@ -119,7 +124,7 @@ class ImageController {
           .json({ error: `Parameter ${isNull[1]} cannot be null or undefined!` });
 
       const image = await prismaClient.image.delete({ where: { id } });
-      await FileOperations.rm(image.path);
+      await FileOperations.rmFile(image.path);
 
       res.sendStatus(200);
     } catch (error) {
--- src/utils/FileOperations.js
+++ src/utils/FileOperations.js
@@ -1,48 +1,84 @@
 import { promises as fs } from "node:fs";
 
+/**
+ * Class for performing file operations.
+ */
 class FileOperations {
-  static async rm(path) {
+  /**
+   * Reads a file and returns its content as a buffer.
+   * @param {string} path - The path to the file.
+   * @returns {Promise<Buffer>} - A promise that resolves to the file content as a buffer.
+   * @throws Will throw an error if the file cannot be read.
+   */
+  static async readFile(path) {
     try {
-      await fs.rm(path, { recursive: true });
+      return await fs.readFile(path);
     } catch (error) {
-      console.error(
-        "[FileOperations] - Error when trying to delete the file: " + path + "\n" + error,
-      );
+      console.error(`[FileOperations] - Error when trying to read the file: ${path}\n${error}`);
+      throw error;
     }
   }
 
-  static async mv(sourcePath, destinationPath) {
+  /**
+   * Deletes a file or directory.
+   * @param {string} path - The path to the file or directory.
+   * @returns {Promise<void>} - A promise that resolves when the file or directory is deleted.
+   * @throws Will throw an error if the file or directory cannot be deleted.
+   */
+  static async rmFile(path) {
+    try {
+      await fs.rm(path, { recursive: true });
+    } catch (error) {
+      console.error(`[FileOperations] - Error when trying to delete the file: ${path}\n${error}`);
+      throw error;
+    }
+  }
+
+  /**
+   * Moves a file from one path to another.
+   * @param {string} sourcePath - The current path of the file.
+   * @param {string} destinationPath - The new path of the file.
+   * @returns {Promise<void>} - A promise that resolves when the file is moved.
+   * @throws Will throw an error if the file cannot be moved.
+   */
+  static async moveFile(sourcePath, destinationPath) {
     try {
       await fs.rename(sourcePath, destinationPath);
     } catch (error) {
       console.error(
-        "[FileOperations] - Error when trying to move the file from " +
-          sourcePath +
-          " to " +
-          destinationPath +
-          "\n" +
-          error,
+        `[FileOperations] - Error when trying to move the file from ${sourcePath} to ${destinationPath}\n${error}`,
       );
+      throw error;
     }
   }
 
+  /**
+   * Checks if a file or directory exists.
+   * @param {string} path - The path to the file or directory.
+   * @returns {Promise<boolean>} - A promise that resolves to true if the file or directory exists, otherwise false.
+   */
   static async exists(path) {
     try {
       await fs.stat(path);
       return true;
-    } catch (error) {
-      console.log("[FileOperations] - File or directory does not exist: " + path);
+    } catch {
+      console.log(`[FileOperations] - File or directory does not exist: ${path}`);
       return false;
     }
   }
 
+  /**
+   * Creates a directory, including any necessary parent directories.
+   * @param {string} path - The path to the directory.
+   * @returns {Promise<void>} - A promise that resolves when the directory is created.
+   * @throws Will throw an error if the directory cannot be created.
+   */
   static async createStoragePath(path) {
     try {
       await fs.mkdir(path, { recursive: true });
     } catch (error) {
-      console.error(
-        "[FileOperations] - Error when trying to create storage: " + path + "\n" + error,
-      );
+      console.error(`[FileOperations] - Error when trying to create storage: ${path}\n${error}`);
+      throw error;
     }
   }
 }
